ae3fabd00de4cfbcb23539325c62d001f9fd76a3
Melika <melikadamirchi2000@gmail.com>
2022-08-25 09:26:36 +0430

    Deployed website

--- src/BitcoinAxios/App.jsx
+++ src/BitcoinAxios/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Body from './Body'
 import Header from './Header'
 import { useState } from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
  
     <div>
-      <h1>salam</h1>
+     
         <Header inn={inn} setInn={setInn} fil={fil} setFil={setFil}/>
         <Body/>
     </div>
